b1cb5af1acefd0b5314da867be819698a46fe246
Алексей <defers86@gmail.com>
2022-02-12 01:45:50 +0300

    t

--- src/components/HomePage/HomePage.component.jsx
+++ src/components/HomePage/HomePage.component.jsx
@@ -13,6 +13,45 @@ const HomePage = () => {
                     <p>Дизайн квартир | ремонт квартир</p>
                 </div>
 
+                <div className = {s.homepage_advantages}>
+                    <div className = {s.advantage}>
+                        <div className = {s.advantage_header}>
+                            <p>Полный спектр работ</p>
+                        </div> 
+                        <div className = {s.advantage_description}>
+                            <p>Дизайн, Ремонт, Надзор, Подбор и Заказ материалов</p>
+                        </div>
+                        <div className = {s.advantage_border}></div>  
+                    </div>
+                    <div className = {s.advantage}>
+                        <div className = {s.advantage_header}>
+                            <p>2 года гарантии</p>
+                        </div> 
+                        <div className = {s.advantage_description}>
+                            <p>Даем гарантию на отделочные работы</p>
+                        </div>
+                        <div className = {s.advantage_border}></div> 
+                    </div>
+                    <div className = {s.advantage}>
+                        <div className = {s.advantage_header}>
+                            <p>Преимущество 3</p>
+                        </div> 
+                        <div className = {s.advantage_description}>
+                            <p>Описание преимущества 3</p>
+                        </div>
+                        <div className = {s.advantage_border}></div> 
+                    </div>
+                    <div className = {s.advantage}>
+                        <div className = {s.advantage_header}>
+                            <p>Преимущество 4</p>
+                        </div> 
+                        <div className = {s.advantage_description}>
+                            <p>Описание преимущества 4</p>
+                        </div>
+                        <div className = {s.advantage_border}></div> 
+                    </div>
+                </div>
+
             </div>
 
         </div>
